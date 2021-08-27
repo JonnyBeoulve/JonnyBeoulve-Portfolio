40378c52617cc0ebab29adcdb0009dd51a5ad647
JonnyBeoulve <jleack@gmail.com>
2021-08-26 17:49:45 -0700

    Title update.

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   heading: 'Jonathan Leack',
-  subHeading: 'Full Stack Software Engineer',
+  subHeading: 'Software Engineer',
   socialLinks: [
     {
       style: 'brands',
